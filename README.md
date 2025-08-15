ba391bfc7b2a9fc2098bb689880a06de7732747a
Edith Wanja <edithwanjohi254@gmail.com>
2025-08-15 20:20:47 +0300

    Add index.html and script.js to create the application that generates content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # alx_fe_javascript
+
+# TASK 0
+
+-- Develop a web application that dynamically generates content based on user input and interactions. 
---
+++ dom-manipulation/script.js
@@ -0,0 +1,19 @@
+const quoteDisplay = document.getElementById('quoteDisplay');
+const quoteButton = document.getElementById(newQuote);
+
+const quoteObject = [
+  { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
+  { text: "In the middle of every difficulty lies opportunity.", category: "Inspiration" },
+  { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+  { text: "If you judge people, you have no time to love them.", category: "Wisdom" },
+  { text: "I have not failed. I've just found 10,000 ways that won't work.", category: "Perseverance" }
+];
+
+function showRandomQuote(){
+
+}
+
+function createAddQuoteForm(){
+    
+}
+
